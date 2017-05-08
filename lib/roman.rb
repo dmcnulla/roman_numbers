a1daad163685d0b2fd87ca410479985659fe76ad
Dave McNulla <mcnulla@gmail.com>
2017-05-07 22:50:46 -0700

    Better number conversion

--- lib/roman.rb
+++ lib/roman.rb
@@ -4,25 +4,25 @@ FIVE = 'V'
 TEN = 'X'
 
 class RomanNumbers
+    def initialize()
+        puts 'Welcome to Roman Numbers!'
+    end
 
     def roman(integer=nil)
         integer ||= @integer
         case integer
-        when 1, 2, 3
+        when 1..3
             return (ONE * integer)
         when 4
             return (ONE + FIVE)
-        when 5
-            return (FIVE)
+        when 5..8
+            return (FIVE + (ONE * (integer - 5)))
+        when 9
+            return (ONE + TEN)
+        when 10
+            return (TEN)
         end
+    else
+        raise "unsupported number: #{integer}"
     end
-end
-
-# save        return (FIVE)
-        end
-    end
-end
-nd
-nd
-end
 end
